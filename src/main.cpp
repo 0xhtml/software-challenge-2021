75059ac568dafbedfe567d66104e3ca4f874b52e
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-11-24 18:04:26 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,20 @@
 
 int main() {
     auto gameState = GameState{};
-    for (int i = 0; i < 7; ++i) {
-        std::vector<Move> possibleMoves = gameState.getPossibleMoves();
-        std::cout << possibleMoves.size() << std::endl;
+    for (int i = 0; i < 20; ++i) {
+        std::vector<Move> possibleMoves = gameState.getPossibleMoves([](unsigned char p) { return false; });
         if (!possibleMoves.empty()) {
             gameState.performMove(possibleMoves[0]);
         }
     }
     PRINT_BOARD(gameState);
+
+    Algorithm algorithm = Algorithm{};
+
     auto start = std::chrono::steady_clock::now();
-    (new Algorithm())->run(gameState);
+    algorithm.run(gameState);
     auto end = std::chrono::steady_clock::now();
+
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms";
     return 0;
 }
